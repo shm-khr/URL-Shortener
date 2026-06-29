1b1b6d699a83317af4878971d82ed833074c680f
shm-khr <kharesoham03@gmail.com>
2026-06-30 00:16:31 +0530

    fix DATABASE_URL env var name

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL) # creating a middle man to communicate with the sqlite
 
